a35c54918a92c6f4883de1f20a0614aff6a152a5
Juninho <juninhopeter1@gmail.com>
2024-07-01 19:08:40 +0100

    Added new changes

--- HNGTASK/Controllers/HelloController.cs
+++ HNGTASK/Controllers/HelloController.cs
@@ -21,28 +21,31 @@ namespace HNGTASK.Controllers
             _openWeatherApiKey = openWeatherOptions.Value.ApiKey;
         }
 
+
+
         [HttpGet]
-        public async Task<DataResponse> Get([FromQuery] string visitorsName)
+        public async Task<DataResponse> Get([FromQuery] string visitors_Name)
         {
             DataResponse response = new DataResponse();
             try
             {
+                
                 var temperature = "Unknown";
                 var location = "unknown";
-                var clientIp = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "127.0.0.1";
+                var clientIp = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "8.8.8.8";
+
 
                 using (var httClient = _httpClientFactory.CreateClient())
                 {
-                    var locationUrl = $"http://api.openweathermap.org/geo/1.0/direct?q={clientIp}&appid={_openWeatherApiKey}";
-                    var locationResponse = await httClient.GetStringAsync(locationUrl);
-                    var locationData = JArray.Parse(locationResponse).FirstOrDefault();
+                    var query = $"http://ip-api.com/json/{clientIp}";
+                    var locationResponse = await httClient.GetStringAsync(query);
+                    var locationData = JsonDocument.Parse(locationResponse).RootElement;
 
-                    if (locationData != null)
+                    if (locationData.TryGetProperty("lat", out var latElement) && locationData.TryGetProperty("lon", out var lonElement))
                     {
-                        location = locationData["name"].ToString();
-                        var lat = locationData["lat"].ToString();
-                        var lon = locationData["lon"].ToString();
-
+                        var lat = latElement.GetDouble();
+                        var lon = lonElement.GetDouble();
+                        location = locationData.GetProperty("city").GetString() ?? "Unknown";
 
                         var weatherUrl = $"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={_openWeatherApiKey}";
                         var weatherResponse = await httClient.GetStringAsync(weatherUrl);
@@ -55,7 +58,7 @@ namespace HNGTASK.Controllers
 
                 response.client_ip = clientIp;
                 response.location = location;
-                response.greeting = $"Hello {visitorsName}, your temperature is {temperature} in {response.location}";
+                response.greeting = $"Hello, {visitors_Name}!, the temperature is {temperature} degree Celcius in {response.location}";
             }
             catch (Exception)
             {
@@ -64,49 +67,5 @@ namespace HNGTASK.Controllers
             }
             return response;
         }
-
-        //[HttpGet]
-        //public async Task<DataResponse> Get([FromQuery] string visitors_Name)
-        //{
-        //    DataResponse response = new DataResponse();
-        //    try
-        //    {
-        //        var temperature = "Unknown";
-        //        var location = "unknown";
-        //        var clientIp = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "8.8.8.8";
-
-
-        //        using (var httClient = _httpClientFactory.CreateClient())
-        //        {
-        //            var query = $"http://ip-api.com/json/{clientIp}";
-        //            var locationResponse = await httClient.GetStringAsync(query);
-        //            var locationData = JsonDocument.Parse(locationResponse).RootElement;
-
-        //            if (locationData.TryGetProperty("lat", out var latElement) && locationData.TryGetProperty("lon", out var lonElement))
-        //            {
-        //                var lat = latElement.GetDouble();
-        //                var lon = lonElement.GetDouble();
-        //                location = locationData.GetProperty("city").GetString() ?? "Unknown";
-
-        //                var weatherUrl = $"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={_openWeatherApiKey}";
-        //                var weatherResponse = await httClient.GetStringAsync(weatherUrl);
-        //                var weatherData = JObject.Parse(weatherResponse);
-        //                temperature = weatherData["main"]["temp"].ToString();
-        //            }
-
-        //        }
-
-
-        //        response.client_ip = clientIp;
-        //        response.location = location;
-        //        response.greeting = $"Hello, {visitors_Name}!, the temperature is {temperature} degree Celcius in {response.location}";
-        //    }
-        //    catch (Exception)
-        //    {
-
-
-        //    }
-        //    return response;
-        //}
     }
 }
